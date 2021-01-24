3c3e320cd916e655bdf3916bc3ab7e3042d381dd
Rukako <secret>
2021-01-24 09:47:56 +0000

    improved the ship page, added sorting by id and name, added categories

--- Main.hs
+++ Main.hs
@@ -1,17 +1,32 @@
+#!/bin/env runhaskell
+
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
-import Control.Monad (forM_)
+import Prelude (IO, Either(Left, Right), String, return, mapM, ($), (++), mapM_, show, error, Bool(False, True), (.), writeFile, const, id, readFile, (==), snd, Int, read)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
-import Data.HashMap.Strict
+import Data.HashMap.Strict ((!))
 import qualified Data.Aeson as Aeson
-import System.Directory (listDirectory)
-import Data.Text (unpack)
-import Data.List (sort)
+import System.IO.Error (catchIOError)
+import System.Directory (listDirectory, removeDirectoryRecursive, createDirectory)
+import Data.Text (pack, unpack)
+import Data.List (sort, sortOn, sortBy, groupBy, head)
+import Text.Read (readEither)
+import Data.Ord (comparing)
 
+(!!) :: (H.Html -> H.Html)
+     -> H.Attribute
+     -> H.Html
+     -> H.Html
+(!!) = (H.!)
+
+hull :: String
+     -> String
 hull = ((++) "/images/hull/") . hull'
 
+hull' :: String
+      -> String
 hull' "Aircraft Carrier"           = "cv.png"
 hull' "Light Aircraft Carrier"     = "cvl.png"
 hull' "Destroyer"                  = "dd.png"
@@ -27,12 +42,18 @@ hull' "Submarine Aircraft Carrier" = "ssv.png"
 hull' "Repair Ship"                = "ar.png"
 hull' "Repair"                     = "ar.png"
 
+issub :: String
+      -> Bool
 issub "Submarine"                  = True
 issub "Submarine Aircraft Carrier" = True
 issub _                            = False
 
+navy :: String
+     -> String
 navy = ((++) "images/navy/") . navy'
 
+navy' :: String
+      -> String
 navy' "Eagle Union"         = "uss_icon.png"
 navy' "Sakura Empire"       = "ijn_icon.png"
 navy' "Royal Navy"          = "hms_icon.png"
@@ -49,57 +70,98 @@ navy' "Iris Libre"          = "ffnf_icon.png"
 navy' "Universal"           = "univ_icon.png"
 navy' "Neptunia"            = "hdn_icon.png"
 navy' "Utawarerumono"       = "uwrr_icon.png"
-navy' "KizunaAI"            = "uwrr_icon.png" -- mistake?
+navy' "KizunaAI"            = "uwrr_icon.png"
 navy' "Bilibili"            = "bili_icon.png"
-navy' "Hololive"            = "uwrr_icon.png" -- mistake?
+navy' "Hololive"            = "uwrr_icon.png"
 
-noimg x = x
-
-mkhtml prefix name x
-  = writeFile (prefix ++ name ++ ".html")
-    $ renderHtml
-    $ H.docTypeHtml
-    $ do H.head $ H.title (H.toHtml name)
-         H.body $ do H.h1 (H.toHtml name)
-                     x
+mkhtml prefix name title x
+  = do css <- readFile "main.css"
+       writeFile (prefix ++ name ++ ".html")
+         $ renderHtml
+         $ H.docTypeHtml
+         $ do H.head
+                $ do H.title (H.toHtml name)
+                     H.style !! A.type_ "text/css" $ H.toHtml css
+              H.body x
 
 showship x
   = do name <- return $ "Ships/" ++ x
        a <- (Aeson.eitherDecodeFileStrict' name :: IO (Either String Aeson.Object))
        case a of
          Left e -> error $ name ++ ": " ++ e
-         Right y -> mkhtml "out/ships/" (ashow $ y ! "name_reference") $ showship' y
-       return x
+         Right json -> do name <- return $ ashow $ json ! "name_reference"
+                          mkhtml "out/ships/" name (ashow $ json ! "name") $ showship' json
+                          return (name, json)
 
-showship' x
+showship' :: Aeson.Object
+          -> H.Html
+showship' json
   = do H.table
-         $ do d "cn_reference"
-              d "nameJP"
-  where d = displayRow x
+         $ do H.thead
+                $ do H.tr $ H.th !! A.scope "col" !! A.colspan "2" $ H.toHtml $ ashow $ json ! "name"
+                     H.tr
+                       $ do H.th !! A.scope "col" $ "Property"
+                            H.th !! A.scope "col" $ "Value"
+              H.tbody
+                $ do d "ID"          "ID"
+                     d "hull"        "Hull"
+                     d "navy"        "Navy"
+                     d "initialStar" "Initial Rating"
+                     d "rarity"      "Rarity"
+  where d = displayRow json
 
-displayRow b a
+displayRow :: Aeson.Object
+           -> String
+           -> String
+           -> H.Html
+displayRow json field text
   = H.tr
-    $ do H.th $ H.toHtml a
-         H.th $ H.toHtml $ ashow $ b ! a
+    $ do H.th !! A.scope "row" $ H.toHtml text
+         H.th $ H.toHtml $ ashow $ json ! pack field
 
-ashow :: Aeson.Value -> String
+ashow :: Aeson.Value
+      -> String
 ashow (Aeson.String x) = unpack x
 ashow (Aeson.Number x) = show x
 ashow (Aeson.Bool x) = show x
 ashow x = show x
 
-makeMainIndex ships
-  = mkhtml "out/" "index" $ H.ol $ mapM_ (\x -> H.li $ H.a H.! A.href (H.stringValue $ "ships/" ++ x) $ H.toHtml x) ships
+makeMainIndex :: String
+              -> String
+              -> [(String, Aeson.Object)]
+              -> IO ()
+makeMainIndex file title ships
+  = mkhtml "out/" file title
+    $ H.ol
+    $ mapM_ (\(name, json) -> H.li $ H.a !! A.href (H.stringValue $ "ships/" ++ name ++ ".html") $ H.toHtml name) ships
 
-makeIndex x y z
-  = return ()
+makeIndex :: String
+          -> (String -> String)
+          -> [(String, Aeson.Object)]
+          -> IO ()
+makeIndex category f ships
+  = do createDirectory $ "out/" ++ category
+       mapM_ (\x -> do name <- return $ ashow $ (snd $ head x) ! (pack category)
+                       mkhtml ("out/" ++ category ++ "/") name name
+                         $ H.ol
+                         $ mapM_ (\(name, json) -> H.li $ H.a !! A.href (H.stringValue $ "../ships/" ++ name ++ ".html") $ H.toHtml name) x) groupedShips
+  where
+    groupedShips :: [[(String, Aeson.Object)]]
+    groupedShips = groupBy (\(_, a) -> \(_, b) -> a ! (pack category) == b ! (pack category)) $ sortBy (comparing $ \a -> ashow $ (snd a) ! (pack category)) ships
 
 main :: IO ()
 main
-  = do dir <- listDirectory "Ships"
+  = do catchIOError (removeDirectoryRecursive "out") $ const $ return ()
+       createDirectory "out"
+       createDirectory "out/ships"
+       dir <- listDirectory "Ships"
        ships <- mapM showship $ sort dir
-       makeMainIndex ships
-       makeIndex "hull" hull ships
-       makeIndex "navy" navy ships
-       makeIndex "class" noimg ships
+       makeMainIndex "index_alphabetic" "Index (Alphabetic)" ships
+       makeMainIndex "index_id"         "Index (By ID)"
+         $ sortOn (\(_, json) -> case readEither (ashow $ json ! "ID") :: Either String Int of
+                                   Left _ -> 0
+                                   Right x -> x) ships
+       makeIndex "rarity" id   ships
+       makeIndex "hull"   hull ships
+       makeIndex "navy"   navy ships
        return ()
