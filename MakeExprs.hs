3145fe660d19aa4b4cb2a4b390e6fda46c001257
Rukako <19629-Rukako@users.noreply.gitgud.io>
2021-05-29 15:48:46 +0000

    fixed

--- MakeExprs.hs
+++ MakeExprs.hs
@@ -15,11 +15,11 @@ main
                  $ filter (notElem '-') base
        mapM_ (\[base, _] -> do out <- readCreateProcess (shell
                                                           $ "compare -metric RMSE -subimage-search assets/painting/" ++ base ++ ".png assets/paintingface/" ++ base ++ "/1.png diff.png 2>&1 >/dev/null | cut -d' ' -f4 | tr ',' +") ""
-                               putStrLn $ "FOR " ++ base ++ ": " ++ out
+                               putStrLn $ "FOR " ++ base ++ ": |" ++ (init out) ++ "|"
                                applyon <- getDirectoryContents ("assets/paintingface/" ++ base)
                                mapM_ (\[i, _]
                                       -> readCreateProcess (shell
-                                                             $ "composite -geometry +" ++ out ++ " assets/paintingface/" ++ base ++ "/" ++ i ++ ".png assets/painting/" ++ base ++ ".png assets/painting/" ++ base ++ "-" ++ i ++ ".png") "")
+                                                             $ "composite -geometry +" ++ (init out) ++ " assets/paintingface/" ++ base ++ "/" ++ i ++ ".png assets/painting/" ++ base ++ ".png assets/painting/" ++ base ++ "-" ++ i ++ ".png") "")
                                  $ map (endBy ".")
                                  $ filter ((<) 2 . length) applyon)
          $ action
