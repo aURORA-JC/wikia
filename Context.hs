f12fffa93348896d59dd9c50f173651a43b7859e
Rukako <19629-Rukako@users.noreply.gitgud.io>
2022-09-08 17:11:29 +0000

    forgot to add context

---
+++ Context.hs
@@ -0,0 +1,22 @@
+module Context where
+
+import Data
+
+data Context
+  = Context
+  {
+    ctx_luaskin :: [Expr],
+    ctx_luaskinextra :: [Expr],
+    ctx_namecode :: [[(Int, (String, String))]],
+    ctx_encn :: [(String, (String, String))],
+    ctx_skins :: [(Int, String, Expr)],
+    ctx_json :: Expr,
+    ctx_ships :: [Expr],
+    ctx_ship_data_template :: Expr,
+    ctx_skill_data_template :: Expr,
+    ctx_ship_data_blueprint :: Expr,
+    ctx_ship_strengthen_blueprint :: Expr,
+    ctx_ship_data_breakout :: Expr,
+    ctx_ship_strengthen_meta :: Expr,
+    ctx_ship_meta_breakout :: Expr
+  }
